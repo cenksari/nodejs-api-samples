2c18b279b43a36c721c8940e3ca32dc8202e4a42
cenksari <cenk@cenksari.com>
2018-02-20 13:01:48 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# NodeJS Simple API Example
+NodeJS API playground.
---
+++ index.js
@@ -0,0 +1,127 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const basicAuthorization = require('express-basic-auth');
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+const jsonParser = bodyParser.json();
+const urlEncodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Basic Authorization
+app.use(basicAuthorization({
+    authorizer: myBasicAuthorizer,
+    authorizeAsync: true,
+    unauthorizedResponse: getUnauthorizedResponse
+}))
+
+function myBasicAuthorizer(username, password, cb) {
+    if (username.startsWith('username') && password.startsWith('secret')) {
+        return cb(null, true);
+    }
+    else {
+        return cb(null, false);
+    }
+}
+
+function getUnauthorizedResponse(req) {
+    return req.auth
+    ? ('Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected')
+    : 'No credentials provided'
+}
+
+// Get query string parameters
+app.get('/api/querystring', urlEncodedParser, (req,res) => {
+    const id = req.query['id'];
+    const name = req.query['name'];
+
+    res.send(id + ' ' + name);
+});
+
+
+// User database
+let users = [
+    { "id": 1, "name" : "John", "lastName": "PETRUCCI", "email": "jpetrucci@dreamtheater.net" },
+    { "id": 2, "name": "John", "lastName": "MYUNG", "email": "jmyung@dreamtheater.net" },
+    { "id": 3, "name": "James", "lastName": "LABRIE", "email": "jlabrie@dreamtheater.net" },
+    { "id": 3, "name": "Jordan", "lastName": "RUDESS", "email": "jrudess@dreamtheater.net" }
+];
+
+// List users
+app.get('/api/users/list', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    res.send(users);
+});
+
+// Get user
+app.get('/api/users/get/:id', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const id = req.params.id;
+
+    const selectedUser = users.filter(function (user) {
+        return user.id == id
+    });
+
+    res.send(selectedUser);
+});
+
+// Add user
+app.post('/api/users/add', jsonParser, (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    if (!req.body || req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        return res.sendStatus(400);
+    }
+
+    const user = req.body;
+
+    users.push(user);
+
+    res.send(users);
+});
+
+// Edit user
+app.post('/api/users/edit', jsonParser, (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    if (!req.body || req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        return res.sendStatus(400);
+    }
+
+    const id = req.body.id;
+    const name = req.body.name;
+
+    for (let i in users) {
+        if (users[i].id == id) {
+            users[i].name = name;
+            break;
+        }
+    }
+
+    res.send(users);
+});
+
+// Delete user
+app.post('/api/users/delete/:id', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const id = req.params.id;
+
+    const filteredUsers = users.filter(function (user) {
+        return user.id != id
+    });
+
+    users = filteredUsers;
+
+    res.send(users);
+});
+
+// Start server
+const server = app.listen(port, function() {
+    console.log('Server started.');
+});
